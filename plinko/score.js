f1794bfd6828d50a77c6637f5ddaa754c46a9690
jp <jp.san.gabriel01@gmail.com>
2018-12-07 21:52:01 +0800

    Remove console.log in onScoreUpdate()

--- plinko/score.js
+++ plinko/score.js
@@ -4,8 +4,6 @@ const outputs = [];
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   outputs.push([dropPosition, bounciness, size, bucketLabel]);
-
-  console.log(outputs);
 }
 
 function runAnalysis() {
